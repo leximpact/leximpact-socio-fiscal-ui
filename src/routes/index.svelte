e4bb406433e5a907908e172a62ebde4857b73613
Emmanuel Raviart <emmanuel@raviart.com>
2021-03-24 08:38:14 +0100

    Prettify.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,54 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+  import Counter from "$lib/Counter.svelte";
 </script>
 
-<main>
-	<h1>Hello world!</h1>
+<style lang="postcss">
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
 
-	<Counter />
+  main {
+    @apply text-center;
+    @apply p-4;
+    @apply mx-auto;
+  }
 
-	<p>Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
+  h1 {
+    @apply text-red-600;
+    @apply uppercase;
+    @apply text-6xl;
+    @apply font-thin;
+    @apply leading-tight;
+    @apply my-16 mx-auto;
+    @apply max-w-xs;
+  }
 
-<style style lang="postcss">
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-	}
+  p {
+    @apply max-w-xs;
+    @apply my-8 mx-auto;
+    @apply leading-snug;
+  }
 
-	main {
-		@apply text-center;
-		@apply p-4;
-		@apply mx-auto;
-	}
+  @screen sm {
+    h1 {
+      @apply max-w-none;
+    }
 
-	h1 {
-		@apply text-red-600;
-		@apply uppercase;
-		@apply text-6xl;
-		@apply font-thin;
-		@apply leading-tight;
-		@apply my-16 mx-auto;
-		@apply max-w-xs;
-	}
-
-	p {
-		@apply max-w-xs;
-		@apply my-8 mx-auto;
-		@apply leading-snug;
-	}
-
-	@screen sm {
-		h1 {
-			@apply max-w-none;
-		}
-
-		p {
-			@apply max-w-none;
-		}
-	}
+    p {
+      @apply max-w-none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello world!</h1>
+
+  <Counter />
+
+  <p>
+    Visit
+    <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>

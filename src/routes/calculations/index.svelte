6b4a953c6a2b9437bb8f6a29197953323aa31bb3
Emmanuel Raviart <emmanuel@raviart.com>
2021-03-29 16:58:30 +0200

    Use WebSocket to compute simulation.

--- src/routes/calculations/index.svelte
+++ src/routes/calculations/index.svelte
@@ -1,43 +1,123 @@
 <script context="module" lang="ts">
-  import type { LoadInput, LoadOutput } from "@sveltejs/kit/types.internal"
+  // import type { LoadInput, LoadOutput } from "@sveltejs/kit/types.internal"
 
-  import { decomposition, walkDecomposition } from "$lib/decompositions"
+  // import { decomposition, walkDecomposition } from "$lib/decompositions"
+  // import type { Situation } from "$lib/situations"
+
+  // // See https://github.com/cbenz/openfisca-interactive/blob/master/waterfall.ipynb
+  // const year = 2017
+  // const variablesDecomposition = Object.fromEntries(
+  //   [...walkDecomposition(decomposition)]
+  //     .filter(
+  //       (node) =>
+  //         ![
+  //           "aide_embauche_pme",
+  //           "aide_premier_salarie",
+  //           "allegement_fillon",
+  //           "cotisations_employeur",
+  //           "cotisations_salariales",
+  //           "cout_du_travail",
+  //           "crds_salaire",
+  //           "credit_impot_competitivite_emploi",
+  //           "csg_deductible_salaire",
+  //           "csg_imposable_salaire",
+  //           "irpp",
+  //           "minima_sociaux",
+  //           "ppa",
+  //           "ppe",
+  //           "prestations_sociales",
+  //           "revenus_nets_du_travail",
+  //           "rsa",
+  //           "salaire_de_base",
+  //           "salaire_imposable",
+  //           "salaire_net",
+  //           "salaire_super_brut",
+  //           "salaire_super_brut_hors_allegements",
+  //           "tehr",
+  //         ].includes(node.code),
+  //     )
+  //     .map((node) => [node.code, { [year]: null }]),
+  // )
+  // const situation: Situation = {
+  //   individus: {
+  //     Claude: {
+  //       salaire_de_base: {
+  //         [year]: 20000,
+  //       },
+  //     },
+  //     Dominique: {
+  //       salaire_de_base: {
+  //         [year]: 30000,
+  //       },
+  //     },
+  //     Camille: {},
+  //   },
+  //   menages: {
+  //     menage_1: {
+  //       personne_de_reference: ["Claude"],
+  //       conjoint: ["Dominique"],
+  //       enfants: ["Camille"],
+  //       ...variablesDecomposition,
+  //     },
+  //   },
+  //   familles: {
+  //     famille_1: {
+  //       parents: ["Claude", "Dominique"],
+  //       enfants: ["Camille"],
+  //     },
+  //   },
+  //   foyers_fiscaux: {
+  //     foyer_fiscal_1: {
+  //       declarants: ["Claude", "Dominique"],
+  //       personnes_a_charge: ["Camille"],
+  //     },
+  //   },
+  // }
+
+  // export async function load({ fetch }: LoadInput): Promise<LoadOutput> {
+  //   const url = "https://fr.openfisca.org/api/latest/calculate"
+  //   const res = await fetch(url, {
+  //     body: JSON.stringify(situation, null, 2),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     method: "POST",
+  //   })
+
+  //   if (!res.ok) {
+  //     console.error(
+  //       `Error ${res.status} while calculating at ${url}\n${JSON.stringify(
+  //         situation,
+  //         null,
+  //         2,
+  //       )}\n\n${await res.text()}`,
+  //     )
+  //     return {
+  //       status: res.status,
+  //       error: new Error(`Could not load ${url}`),
+  //     }
+  //   }
+  //   return {
+  //     props: {
+  //       simulation: await res.json(),
+  //     },
+  //   }
+  // }
+</script>
+
+<script lang="ts">
+  import Sockette from "sockette"
+
+  import { browser } from "$app/env"
+  import { session } from "$app/stores"
+  import { decomposition } from "$lib/decompositions"
+  // import type { Simulation } from "$lib/simulations"
   import type { Situation } from "$lib/situations"
 
-  // See https://github.com/cbenz/openfisca-interactive/blob/master/waterfall.ipynb
+  // export let simulation: Simulation
+
+  let results: { code: string; value: number[] }[] = []
   const year = 2017
-  const variablesDecomposition = Object.fromEntries(
-    [...walkDecomposition(decomposition)]
-      .filter(
-        (node) =>
-          ![
-            "aide_embauche_pme",
-            "aide_premier_salarie",
-            "allegement_fillon",
-            "cotisations_employeur",
-            "cotisations_salariales",
-            "cout_du_travail",
-            "crds_salaire",
-            "credit_impot_competitivite_emploi",
-            "csg_deductible_salaire",
-            "csg_imposable_salaire",
-            "irpp",
-            "minima_sociaux",
-            "ppa",
-            "ppe",
-            "prestations_sociales",
-            "revenus_nets_du_travail",
-            "rsa",
-            "salaire_de_base",
-            "salaire_imposable",
-            "salaire_net",
-            "salaire_super_brut",
-            "salaire_super_brut_hors_allegements",
-            "tehr",
-          ].includes(node.code),
-      )
-      .map((node) => [node.code, { [year]: null }]),
-  )
   const situation: Situation = {
     individus: {
       Claude: {
@@ -57,7 +137,6 @@
         personne_de_reference: ["Claude"],
         conjoint: ["Dominique"],
         enfants: ["Camille"],
-        ...variablesDecomposition,
       },
     },
     familles: {
@@ -74,46 +153,6 @@
     },
   }
 
-  export async function load({ fetch }: LoadInput): Promise<LoadOutput> {
-    const url = "https://fr.openfisca.org/api/latest/calculate"
-    const res = await fetch(url, {
-      body: JSON.stringify(situation, null, 2),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    })
-
-    if (!res.ok) {
-      console.error(
-        `Error ${res.status} while calculating at ${url}\n${JSON.stringify(
-          situation,
-          null,
-          2,
-        )}\n\n${await res.text()}`,
-      )
-      return {
-        status: res.status,
-        error: new Error(`Could not load ${url}`),
-      }
-    }
-    return {
-      props: {
-        simulation: await res.json(),
-      },
-    }
-  }
-</script>
-
-<script lang="ts">
-  import Sockette from "sockette"
-
-  import { browser } from "$app/env"
-  import { session } from "$app/stores"
-  import type { Simulation } from "$lib/simulations"
-
-  export let simulation: Simulation
-
   let webSocket: Sockette | undefined = undefined
 
   $: if (browser) {
@@ -127,6 +166,12 @@
         // maxAttempts: 10,
         onmessage: (event) => {
           console.log("WebSocket message received:", event)
+          const result = JSON.parse(event.data)
+          if (result.error !== undefined) {
+            console.log("Error:", result)
+          } else {
+            results = [...results, result]
+          }
         },
         // onopen: (event) => console.log("[WebSocket] Connected!", event),
         // onreconnect: (event) =>
@@ -141,10 +186,26 @@
   }
 
   function submit() {
-    webSocket.send("Kilroy was here!")
+    results = []
+    webSocket.send(
+      JSON.stringify({
+        decomposition,
+        situation,
+      }),
+    )
+    webSocket.send(
+      JSON.stringify({
+        calculate: true,
+      }),
+    )
   }
 </script>
 
-<button on:click={submit}>Click me</button>
+<pre>{JSON.stringify(situation)}</pre>
 
-<pre>{JSON.stringify(simulation, null, 2)}</pre>
+<button on:click={submit}>Simuler</button>
+
+{#each results as result}
+  <pre>{JSON.stringify(result)}</pre>
+{/each}
+<!-- <pre>{JSON.stringify(simulation, null, 2)}</pre> -->
--- src/routes/variables/index.svelte
+++ src/routes/variables/index.svelte
@@ -24,6 +24,7 @@
   export let variableById: VariableSummaryById
 </script>
 
+<h1>Variables</h1>
 <ul class="list-disc list-inside">
   {#each Object.entries(variableById) as [id, variable]}
     <li>

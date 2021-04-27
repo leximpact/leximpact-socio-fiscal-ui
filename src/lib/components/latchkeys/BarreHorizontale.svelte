a3a1f07ed74f367d7bf1ab19327f3398af0f6c8c
Dorine Lambinet <dorine.lambinet@gmail.com>
2021-04-27 17:30:09 +0200

    Création des composants svelte SVG

---
+++ src/lib/components/latchkeys/BarreHorizontale.svelte
@@ -0,0 +1,16 @@
+<svg
+  width="183"
+  height="59"
+  viewBox="0 0 183 59"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <rect
+    x="183"
+    width="59"
+    height="183"
+    transform="rotate(90 183 0)"
+    fill="#CFD8DB"
+    fill-opacity="0.5"
+  />
+</svg>
